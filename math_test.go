2354a882f31489662b51b9f3e5d9046fa2ede014
Animesh Chaudhry <animesh.chaudhry1997@gmail.com>
2021-02-17 15:38:16 -0800

    added go tests

---
+++ math_test.go
@@ -0,0 +1,23 @@
+package math
+
+import "testing"
+
+func TestAdd(t *testing.T){
+
+    got := Add(4, 6)
+    want := 10
+
+    if got != want {
+        t.Errorf("got %q, wanted %q", got, want)
+    }
+}
+
+func TestSubtract(t *testing.T){
+
+    got := Subtract(10, 2)
+    want := 8
+
+    if got != want {
+        t.Errorf("got %q, wanted %q", got, want)
+    }
+}
